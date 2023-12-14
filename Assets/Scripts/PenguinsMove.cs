547bcde314744b0829b8f91792c2e60138e72565
Cansel Karabulut <canselkarabulutt@gmail.com>
2023-12-14 14:45:43 +0300

    penguin move

--- Assets/Scripts/PenguinsMove.cs
+++ Assets/Scripts/PenguinsMove.cs
@@ -1,12 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PenguinsMove : MonoBehaviour
 {
-    public float speed=5;
+    public GameObject penguinTargets;
+    public NavMeshAgent myNavMeshAgent;
+    private int _targetCount;
+    private int _selectedTargetPointNum;
+    private Transform _selectedPointTransform;
+    private float _dist;
+
+    private void Start()
+    {
+        _targetCount = penguinTargets.transform.childCount;
+        RandomTarget();
+    }
     void Update()
     {
-        transform.position += new Vector3(0, 0, 1*Time.deltaTime*speed);
+        _dist = Vector3.Distance(transform.position, _selectedPointTransform.position);
+    }
+    IEnumerator Target()
+    {
+        yield return new WaitUntil(() => _dist < 0.7f);
+        RandomTarget();
+    }
+    void RandomTarget()
+    {
+        _selectedTargetPointNum = Random.Range(0, _targetCount);
+        _selectedPointTransform = penguinTargets.transform.GetChild(_selectedTargetPointNum);
+        SetDestinationPosition();
+    }
+    void SetDestinationPosition()
+    {
+        myNavMeshAgent.SetDestination(_selectedPointTransform.position);
+        StartCoroutine(Target());
     }
 }
