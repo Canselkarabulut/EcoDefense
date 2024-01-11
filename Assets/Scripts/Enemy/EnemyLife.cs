f44e5ae159d9836bd4c50363682e5215acd88a14
Cansel Karabulut <canselkarabulutt@gmail.com>
2024-01-11 15:58:36 +0300

    Revert "Uı edit , new ground"
    This reverts commit 375a3149d7d8557aa5c367b7cf6893532a245d68.

--- Assets/Scripts/Enemy/EnemyLife.cs
+++ Assets/Scripts/Enemy/EnemyLife.cs
@@ -8,19 +8,17 @@ using UnityEngine.AI;
 public class EnemyLife : MonoBehaviour
 {
     private EnemyLevelStatus _enemyLevelStatus;
-
     private int _lifeCapacity;
-
-    //   private int _numberBulletsTouchedi=0;
-    private int _numberBulletsHit = 0; //isabet eden mermi sayısı
+ //   private int _numberBulletsTouchedi=0;
+    private int _numberBulletsHit=0; //isabet eden mermi sayısı
     public PlayerRange playerRange;
     public GameObject fireEffect;
     public GameObject dieEffect;
     public GameObject coin;
     public Animator enemyAnimator;
     public bool isDie;
-
-
+    
+    
     private void Awake()
     {
         _enemyLevelStatus = this.gameObject.GetComponent<EnemyLevelStatus>();
@@ -37,49 +35,46 @@ public class EnemyLife : MonoBehaviour
         if (other.TryGetComponent(out Bullet bullet))
         {
             _numberBulletsHit++;
-
+            
             if (_numberBulletsHit == _lifeCapacity - 2)
             {
                 fireEffect.SetActive(true);
             }
-
-            if (_numberBulletsHit == _lifeCapacity - 1)
+            if (_numberBulletsHit == _lifeCapacity-1)
             {
                 GetComponent<NavMeshAgent>().enabled = false;
                 GetComponent<CapsuleCollider>().enabled = false;
-                GetComponent<EnemyMove>().enabled = false;
+               GetComponent<EnemyMove>().enabled = false;
                 GetComponent<EnemyLevelStatus>().enabled = false;
                 GetComponent<EnemyLife>().enabled = false;
                 GetComponent<EnemyTrigger>().enabled = false;
-                enemyAnimator.SetBool("isDie", true);
-
+                enemyAnimator.SetBool("isDie",true);
+              
                 fireEffect.SetActive(false);
                 dieEffect.SetActive(true);
                 isDie = true;
-
+                
                 //kafamın üstünde coin çıksın resim olarak içini yazı ile dolduracağız enemy spawn da belirtilen
                 coin.SetActive(true);
                 StartCoroutine(EnemyDie());
             }
         }
     }
-
+    
     IEnumerator EnemyDie()
     {
         yield return new WaitForSeconds(3);
         DieAnimEnd();
     }
-
+    
     public void DieAnimEnd()
     {
         ObjectPool.Instance.ReturnObjectToPool(gameObject);
         InitializeStart();
     }
-
+    
     public void InitializeStart()
     {
-        transform.position = new Vector3(0,1,0);
-        transform.rotation = Quaternion.Euler(0, 0, 0);
         isDie = false;
         _numberBulletsHit = 0;
         playerRange.smallestDistance = Mathf.Infinity;
@@ -87,7 +82,7 @@ public class EnemyLife : MonoBehaviour
 //        particleEffect.SetActive(true);
         fireEffect.SetActive(false);
         dieEffect.SetActive(false);
-        enemyAnimator.SetBool("isDie", false);
+        enemyAnimator.SetBool("isDie",false);
         GetComponent<NavMeshAgent>().enabled = true;
         GetComponent<CapsuleCollider>().enabled = true;
         GetComponent<EnemyMove>().enabled = true;
@@ -95,25 +90,24 @@ public class EnemyLife : MonoBehaviour
         GetComponent<EnemyLife>().enabled = true;
         GetComponent<EnemyTrigger>().enabled = true;
         coin.SetActive(false);
-
+        
         if (_enemyLevelStatus.EnemyLevelReturn() == EnemyLevel.Lvl1Enemy)
         {
             _lifeCapacity = 4;
         }
-
         if (_enemyLevelStatus.EnemyLevelReturn() == EnemyLevel.Lvl2Enemy)
         {
             _lifeCapacity = 6;
         }
-
         if (_enemyLevelStatus.EnemyLevelReturn() == EnemyLevel.Lvl3Enemy)
         {
             _lifeCapacity = 8;
         }
-
         if (_enemyLevelStatus.EnemyLevelReturn() == EnemyLevel.Lvl4Enemy)
         {
             _lifeCapacity = 10;
         }
     }
+
+   
 }
